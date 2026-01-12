01590aeefe89ccd3df163a8c57587d2e3bf49fa1
shivaraj110 <shivarajchandaragi9@gmail.com>
2026-01-12 23:54:28 +0530

    added auth for hitting github api for avoiding rate limits

--- src/hooks/stars.ts
+++ src/hooks/stars.ts
@@ -2,9 +2,20 @@ export const getStarsCount = async (
   repo: string,
 ): Promise<{ repo: string; stars: number }> => {
   try {
-    const res = await fetch(`https://api.github.com/repos/${repo}`);
+    const headers: HeadersInit = {
+      Accept: "application/vnd.github.v3+json",
+    };
+
+    if (process.env.GITHUB_AUTH_TOKEN) {
+      headers.Authorization = `Bearer ${process.env.GITHUB_AUTH_TOKEN}`;
+    }
+
+    const res = await fetch(`https://api.github.com/repos/${repo}`, {
+      headers,
+    });
     if (res.ok) {
       const data = await res.json();
+
       return { repo: repo, stars: data.stargazers_count ?? 0 };
     }
   } catch {
